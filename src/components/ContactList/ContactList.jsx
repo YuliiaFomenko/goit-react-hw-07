a26e0037da7b29d04a3fbd9716a579226645e988
Yuliia Fomenko <yaulia3@gmail.com>
2025-05-14 19:31:48 +0100

    fixed

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -18,13 +18,13 @@ const ContactList = () => {
 
   return (
     <div>
+      {loading && <span>Loading...</span>}
+      {error && <h2>Server is dead...</h2>}
       <ul className={s.contactList}>
         {filteredContacts.map((contact) => (
           <Contact key={contact.id} id={contact.id} name={contact.name} number={contact.number} />
         ))}
       </ul>
-      {loading && <span>Loading...</span>}
-      {error && <h2>Server is dead...</h2>}
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
-import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
